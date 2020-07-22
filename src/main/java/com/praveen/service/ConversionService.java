59697e3928b1c9db3855b3a6da8ace5de644cc16
Your Name <you@example.com>
2020-07-23 01:34:45 +0530

    Added Service Class Implementation

---
+++ src/main/java/com/praveen/service/ConversionService.java
@@ -0,0 +1,12 @@
+package com.praveen.service;
+
+import com.praveen.domain.ConversionBean;
+
+public interface ConversionService {
+	
+	ConversionBean convertDistance(ConversionBean conversionBean);
+
+    ConversionBean convertTemperature(ConversionBean conversionBean);
+    
+
+}
